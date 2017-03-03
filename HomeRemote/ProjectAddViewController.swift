3226f2bb63a4ea4bf18fe62012eb95cad601af08
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2017-03-02 23:43:01 -0600

    Added dummy screen for adding a custom project

---
+++ HomeRemote/ProjectAddViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectAddViewController.swift
+//  HomeRemote
+//
+//  Created by Lucas McDonald on 3/2/17.
+//  Copyright © 2017 Lucas McDonald. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProjectAddViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
