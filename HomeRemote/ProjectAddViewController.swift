dfb75dc6d7637a8b2dc808032cbe3d0eaf385d13
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2017-03-08 01:47:48 -0600

    Completed UI for ProjectAddVC

--- HomeRemote/ProjectAddViewController.swift
+++ HomeRemote/ProjectAddViewController.swift
@@ -15,10 +15,19 @@ class ProjectAddViewController: UIViewController {
     @IBOutlet weak var projectNicknameField: UITextField!
     @IBOutlet weak var githubField: UITextField!
     @IBOutlet weak var publicSwitch: UISwitch!
+    @IBOutlet weak var contactEmailField: UITextField!
+    @IBOutlet weak var contactEmailLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        publicSwitch.isOn = false
+        
+        // hide fields that should only be visible if the project is to be made public
+        
+        contactEmailLabel.alpha = 0
+        contactEmailField.alpha = 0
     }
     
     @IBAction func publicInfoButtonPressed(_ sender: UIButton) {
@@ -27,7 +36,21 @@ class ProjectAddViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func projectInfoButtonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add Custom Project", message: "This allows you to add any project that you have created. You must provide a valid GitHub link. If the link is valid and you are having issues, check to make sure that you follow the proper project format.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     
+    @IBAction func switchTriggered(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            
+            self.contactEmailField.alpha = 1.0 - self.contactEmailField.alpha
+            self.contactEmailLabel.alpha = 1.0 - self.contactEmailLabel.alpha
+            
+        })
+    }
     
     
 }
