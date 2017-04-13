b847d906ecc608c379c1fefd3cd038eb88637206
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2017-04-13 02:36:50 -0500

    More comments

--- HomeRemote/FirebaseNavigationViewController.swift
+++ HomeRemote/FirebaseNavigationViewController.swift
@@ -2,27 +2,33 @@
 //  FirebaseNavigationVIewController.swift
 //  HomeRemote
 //
+//  Controls the Navigation VC through the Firebase JSON.
+//
 //  Created by Lucas McDonald on 2/25/17.
 //
 
-import Foundation
 import UIKit
+import Foundation
 import Firebase
 
 class FirebaseNavigationViewController: UITableViewController {
     
+    // UI elements
     let cellReuseIdentifier = "cell"
     var tableViewData = [String]()
+    @IBOutlet var tableViewList: UITableView!
+    
+    
+    // Navigational elements
     var ref = FIRDatabase.database().reference().child("devices")
     var prevRef = ""
     var nodeDepth = 0
     
-    @IBOutlet var tableViewList: UITableView!
+    /******************
+       Setup Methods
+    ******************/
     
     override func viewDidLoad() {
-        
-        // load table view stuff
-        
         
         // Register the table view cell class and its reuse id
         self.tableViewList.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
@@ -43,22 +49,6 @@ class FirebaseNavigationViewController: UITableViewController {
         
     }
     
-    // create a cell for each table view row
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // create a new cell if needed or reuse an old one
-        let cell:UITableViewCell = tableViewList.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
-        
-        // set the text from the data model
-        cell.textLabel?.text = tableViewData[indexPath.row]
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableViewData.count
-    }
-    
 
     
     
@@ -74,75 +64,92 @@ class FirebaseNavigationViewController: UITableViewController {
             self.navigationController?.pushViewController(nextVC, animated: true)
         }
             
-            // if this is NOT a navigational node, i.e. links to a GitHub file
+        // if this is NOT a navigational node, i.e. links to a GitHub file
         else {
-            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-            let detailVC = storyBoard.instantiateViewController(withIdentifier: "FirebaseDataViewController") as! FirebaseDataViewController
-            detailVC.prevRef = self.tableViewData[indexPath.row]
-            
-            var retrievedGithubLink = ""
-            
-            self.ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let value = snapshot.value as? NSDictionary
-                retrievedGithubLink = value?[self.tableViewData[indexPath.row]] as? String ?? ""
-                detailVC.githubLink = retrievedGithubLink
-                
-                let requestURL: NSURL = NSURL(string: self.getRawLink(link: retrievedGithubLink))!
-                let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
-                let session = URLSession.shared
-                let task = session.dataTask(with: urlRequest as URLRequest) {
-                    (data, response, error) -> Void in
-                    
-                    let httpResponse = response as! HTTPURLResponse
-                    let statusCode = httpResponse.statusCode
-                    
-                    if (statusCode == 200) {
-                        
-                        do{
-                            
-                            let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as! [String: Any]
-                            
-                            if let fetchedTitle = json["Title"] as? String {
-                                detailVC.titleData = fetchedTitle
-                                print(fetchedTitle)
-                            }
-                            if let fetchedDescription = json["Description"] as? String {
-                                detailVC.descriptionData = fetchedDescription
-                                print(fetchedDescription)
-                            }
-                            if let fetchedAuthor = json["Author"] as? String {
-                                detailVC.authorData = fetchedAuthor
-                                print(fetchedAuthor)
-                            }
-                            if let fetchedRemote = json["Remote Type"] as? String {
-                                detailVC.remoteTypeData = fetchedRemote
-                                print(fetchedRemote)
-                            }
-                        }catch {
-                            print("Error with Json: \(error)")
-                        }
-                        
-                    }
-                    
-                }
-                
-                task.resume()
-                usleep(25000)
-                self.navigationController?.show(detailVC, sender: self)
-                
-            }) { (error) in
-                print(error.localizedDescription)
-            }
-            
+            getGithubInfo(indexPath: indexPath)
         }
-        
-        
-        
-        
-        
     }
     
+    
+    /**
+     
+     Reads the PhoneInfo.json file and parses it to update UI elements in the FirebaseDataVC.
+     
+    **/
+    func getGithubInfo(indexPath: IndexPath) {
+        // get storyboard and instantiate next VC
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let detailVC = storyBoard.instantiateViewController(withIdentifier: "FirebaseDataViewController") as! FirebaseDataViewController
+        detailVC.prevRef = self.tableViewData[indexPath.row]
+        
+        // Github link for trimming
+        var retrievedGithubLink = value?[self.tableViewData[indexPath.row]] as String
+        detailVC.githubLink = retrievedGithubLink
+        
+        // access the data from Firebase
+        self.ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            // initiate dictionary for storing JSON
+            let value = snapshot.value as? NSDictionary
+            
+            // initiate URL request to access JSON
+            let requestURL: NSURL = NSURL(string: self.getPhoneInfoJsonLink(link: retrievedGithubLink))!
+            let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+            let session = URLSession.shared
+            
+            // initiate asynchronous task to update UI accordingly
+            let task = session.dataTask(with: urlRequest as URLRequest) {
+                (data, response, error) -> Void in
+                
+                let httpResponse = response as! HTTPURLResponse
+                let statusCode = httpResponse.statusCode
+                
+                if (statusCode == 200) {
+                    
+                    do{
+                        
+                        // parse and restore the JSON
+                        let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as! [String: Any]
+                        
+                        // store each respective UI element in the DetailVC
+                        
+                        if let fetchedTitle = json["Title"] as? String {
+                            detailVC.titleData = fetchedTitle
+                            print(fetchedTitle)
+                        }
+                        if let fetchedDescription = json["Description"] as? String {
+                            detailVC.descriptionData = fetchedDescription
+                            print(fetchedDescription)
+                        }
+                        if let fetchedAuthor = json["Author"] as? String {
+                            detailVC.authorData = fetchedAuthor
+                            print(fetchedAuthor)
+                        }
+                        if let fetchedRemote = json["Remote Type"] as? String {
+                            detailVC.remoteTypeData = fetchedRemote
+                            print(fetchedRemote)
+                        }
+                    } catch {
+                        print("Error with Json: \(error)")
+                    }
+                }
+            }
+            
+            // execute async task
+            task.resume()
+            
+            // TEMPORARY FIX. Fixed delay to allow time for the asynchronous task to complete.
+            usleep(30000)
+            
+            // present the detailVC
+            self.navigationController?.show(detailVC, sender: self)
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+
+
     // called to get the node's data from firebase
     func getFirebaseDatabase() {
         
@@ -173,18 +180,39 @@ class FirebaseNavigationViewController: UITableViewController {
         
     }
     
-    func getRawLink(link: String) -> String {
-        if let rangeOfZero = link.range(of: "com", options: .backwards) {
-            // Found a zero
-            var trimmedLink = String(link.characters.suffix(from: rangeOfZero.upperBound)) // "984"
+    /**
+    
+     Gets the link to the PhoneInfo.json file from the GitHub link.
+ 
+    **/
+    func getPhoneInfoJsonLink(link: String) -> String {
+        if let rangeOfCom = link.range(of: "com", options: .backwards) {
+            var trimmedLink = String(link.characters.suffix(from: rangeOfCom.upperBound))
             trimmedLink = "https://raw.githubusercontent.com" + trimmedLink + "/master/PhoneInfo.json"
-            print("trimmed link:" + trimmedLink)
             return trimmedLink
         }
         else{
             print("oopsie")
             return "oop"
         }
+    }
+    
+    
+    
+    // create a cell for each table view row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = tableViewList.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = tableViewData[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewData.count
     }
     
 }
